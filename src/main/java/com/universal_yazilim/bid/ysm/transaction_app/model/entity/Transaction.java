31315c087a2954a89aa593aebd6f81c2f4b76eb4
MedineDemir12 <Medinedemir12@gmail.com>
2023-04-05 11:26:12 +0300

    initial commit

---
+++ src/main/java/com/universal_yazilim/bid/ysm/transaction_app/model/entity/Transaction.java
@@ -0,0 +1,31 @@
+package com.universal_yazilim.bid.ysm.transaction_app.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+@Data
+@SequenceGenerator(name = "TRANSACTIONS_SEQUENCE", sequenceName = "TRANSACTIONS_SEQ", initialValue = 1, allocationSize = 1)
+@Table(name = "TRANSACTIONS2")
+@Entity
+
+public class Transaction {
+    @Column(name = "TRANSACTION_ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TRANSACTIONS_SEQUENCE")
+    @Id
+    private Integer transactionID;
+    @Column(name = "USER_ID", nullable = false)
+    private Integer userID;
+    @Column(name = "PRODUCT_ID", nullable = false)
+    private Integer productID;
+
+    @Column(name = "TRANSACTION_TIME", nullable = false)
+    private Date transactionTime;
+
+}
+
+
+
+
+
+
---
+++ src/main/java/com/universal_yazilim/bid/ysm/transaction_app/model/service/TransactionService.java
@@ -0,0 +1,101 @@
+package com.universal_yazilim.bid.ysm.transaction_app.model.service;
+
+import com.universal_yazilim.bid.ysm.transaction_app.model.entity.Transaction;
+import org.springframework.dao.OptimisticLockingFailureException;
+import com.universal_yazilim.bid.ysm.transaction_app.utility.Util;
+import com.universal_yazilim.bid.ysm.transaction_app.utility.ExceptionMessageType;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class TransactionService extends AbstractTransactionService
+{
+    @Override
+    public List<Transaction> findAllByUserID(Integer userID)
+     //  protected Optional<List<Transaction>>findAllByUserId(Integer userID)
+    {
+        try
+        {
+            return transactionRepository.findAllByUserID(userID)
+                    .orElseThrow(() -> new RuntimeException(ExceptionMessageType.FIND_BY_ID.getValue()));
+
+        }
+        catch (IllegalArgumentException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+        catch (NullPointerException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+        catch (RuntimeException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+    }
+
+
+    @Override
+    public Transaction findByID(Integer id)
+    {
+        try {
+            return transactionRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException(ExceptionMessageType.FIND_BY_ID.getValue()));
+        }
+        catch (IllegalArgumentException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+        catch (NullPointerException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+        catch (RuntimeException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+    }
+
+    @Override
+    public List<Transaction> getAll()
+    {
+        return transactionRepository.findAll();
+    }
+
+    @Override
+    public Transaction save(Transaction entity)
+    {
+        try
+        {
+            return transactionRepository.save(entity);
+        }
+        catch (IllegalArgumentException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+        catch (OptimisticLockingFailureException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+            return null;
+        }
+    }
+    @Override
+    public void deleteByID(Integer id)
+    {
+        try
+        {
+            transactionRepository.deleteById(id);
+        }
+        catch (IllegalArgumentException e)
+        {
+            Util.showGeneralExceptionInfo(e);
+        }
+    }
+}
+
